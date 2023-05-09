7f0e3d4ef90a5482e5fa6fcd07ae7b8d270cb2f4
SametCimen1 <cimensamet338@gmail.com>
2023-05-08 22:10:41 -0400

    dsadsa

---
+++ app.js
@@ -0,0 +1,19 @@
+var slideIndex = 1;
+currentSlide()
+
+function currentSlide(index){
+    if(typeof index === "undefined"){
+        for(let i = 1; i < 4; i++){
+                   document.querySelector(".img"+i).style.display = "none"
+        }
+        document.querySelector(".img"+slideIndex).style.display = "block"
+    }
+    else{
+        for(let i = 1; i < 4; i++){
+                   document.querySelector(".img"+i).style.display = "none"
+        }
+        document.querySelector(".img"+index).style.display = "block"
+    }
+
+
+}
